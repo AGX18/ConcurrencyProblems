b41391b0ca2b371cfec57b1749e5f85ab2ae313d
Omnia Fathy <agxnh2020@gmail.com>
2025-09-18 00:35:57 +0000

    implemented Multi-threaded mergesort

--- multi-sort/mergesort.c
+++ multi-sort/mergesort.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#define NUM_THREADS 4
 
 #include "cputime.h"
+
+typedef struct msort_args {
+  int *arr;
+  int n;
+} msort_args;
+
+void *start(void *arg) {
+  msort_args *args = (msort_args *)arg;
+  msort(args->arr, args->n);
+  return NULL;
+}
 
 void merge(int *arr, int n, int mid) {
   int left = 0, right = mid, i;
@@ -32,6 +45,38 @@ void msort(int *arr, int n) {
   merge(arr, n, mid);
 }
 
+void thread_msort(int *arr, int n) {
+  int num_threads = NUM_THREADS;
+  pthread_t threads[num_threads];
+  msort_args *args_arr[num_threads];
+  
+  for (int i = 0; i < num_threads; i++) {
+      args_arr[i] = malloc(sizeof(msort_args));
+      args_arr[i]->arr = arr + i * (n / num_threads);
+      
+      if (i == num_threads - 1)
+          args_arr[i]->n = n - i * (n / num_threads);
+      else
+          args_arr[i]->n = n / num_threads;
+          
+      pthread_create(&threads[i], NULL, start, (void*)args_arr[i]);
+  }
+  
+  // Wait for all threads to complete
+  for (int i = 0; i < num_threads; i++) {
+      pthread_join(threads[i], NULL);
+      free(args_arr[i]);
+  }
+
+  for (int i = 1; i < num_threads; i++) {
+    if (i == num_threads - 1)
+      merge(arr, n, i * (n / num_threads));
+    else
+      merge(arr, (i + 1) * (n / num_threads), i * (n / num_threads));
+  }
+
+}
+
 int main () {
   int n = 1 << 24;
   int *arr = malloc(n * sizeof(int));
@@ -45,8 +90,10 @@ int main () {
   // actually sort, and time cpu use
   struct profile_times t;
   profile_start(&t);
-  msort(arr, n);
+  thread_msort(arr, n);
   profile_log(&t);
+
+
 
   // assert that the output is sorted
   for (int i = 0; i < n - 1; i++)
@@ -55,5 +102,7 @@ int main () {
              arr[i + 1]);
       return 1;
     }
+
+  free(arr);
     return 0;
 }
